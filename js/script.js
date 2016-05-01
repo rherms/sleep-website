56aa5f964513d582c0fd66f4119128c85c14f60f
rherms <rlhermstein@yahoo.com>
2016-04-30 17:13:06 -0700

    Made more progress on explore. Inputs are all set up

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+	/*
+	Preschoolers (3-5): Sleep range widened by one hour to 10-13 hours (previously it was 11-13)
+School age children (6-13): Sleep range widened by one hour to 9-11 hours (previously it was 10-11)
+Teenagers (14-17): Sleep range widened by one hour to 8-10 hours (previously it was 8.5-9.5)
+Younger adults (18-25): Sleep range is 7-9 hours (new age category)
+Adults (26-64): Sleep range did not change and remains 7-9 hours
+Older adults (65+): Sleep range is 7-8 hours (new age category)  
+*/
+
+}
